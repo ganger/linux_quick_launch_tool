43f2d9a84756a51f5020a5a546bcd44e210f88fa
ganger <=Ganger695988290@gmail.com>
2014-09-05 19:20:24 +0800

    version 1.0

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include"settingwindow.h"
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    SettingWindow sw;
-   // sw.show();
-    return a.exec();
-}
